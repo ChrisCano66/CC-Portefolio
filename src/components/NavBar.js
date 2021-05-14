688c3f44dae2c6c25264f927945691a3be051514
christophe canovas <christophecanovas66@gmail.com>
2021-05-14 17:06:26 +0200

    Initial commit + some component/style/etc

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+// REACT
+import React from 'react'
+
+// IMAGE
+//import xxxxx1 from '../assets/img'
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <div className="xxxx">
+                <img src={"xxxxx1"} alt=""/>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// REACT
+import React from 'react'
+
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Titre</h1>
+        </div>
+    )
+}
+
+export default HomePage;
